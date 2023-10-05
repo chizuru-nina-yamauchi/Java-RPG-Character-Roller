0c46e413154d4cadc36ffad3c1596eea96904cb6
Chizuru <chizuru.yamauchi@dci-student.org>
2023-10-05 16:09:01 +0200

    fixed the peroblem with Shogik's help

--- Creator.java
+++ Creator.java
@@ -3,39 +3,41 @@ import java.util.Random;
 
 public class Creator {
     public static void main(String[] args) {
+        Random random = new Random();
 
-        String characterName = args[0];
+        String [] characterTypes = {"Warrior", "Wizard", "Potato"};
+        String character = characterTypes[random.nextInt(characterTypes.length)];
 
         System.out.println("Welcome to the character generator!");
 
-Random rng = new Random();
 
-int n = rng.nextInt(13);
+int n = random.nextInt(11) + 2;
 
+int Strength = random.nextInt(11) + 2;
+int Magic = random.nextInt(11) + 2;
+int  Health= random.nextInt(11) + 2;
 
-
-        if (characterName.equals("Fred")){
-            System.out.println(characterName + " is .... a Wizard!");
+        if (character.equals("Warrior")){
+            System.out.println(args[0] + " is .... a " + character + "!");
             System.out.println();
-            System.out.println("Strength: " + n);
-            System.out.println("Magic: " + n);
-            System.out.println("Health: " + n);
-        } else if (characterName.equals("Nina")) {
-            System.out.println(characterName + " is .... a Warrior");
+            System.out.println("Strength: " + Strength*3);
+            System.out.println("Magic: " + Magic);
+            System.out.println("Health: " + Health);
+        } else if (character.equals("Wizard")) {
+            System.out.println(args[0] + " is .... a " + character + "!");
             System.out.println();
-            System.out.println("Strength: " + n);
-            System.out.println("Magic: " + n);
-            System.out.println("Health: " + n);
+            System.out.println("Strength: " + Strength);
+            System.out.println("Magic: " + Magic*3);
+            System.out.println("Health: " + Health);
         } else {
-            System.out.println(characterName + " is .... a Potato");
+            System.out.println(args[0] + " is .... a " + character + "!");
             System.out.println();
-            System.out.println("Strength: " + n);
-            System.out.println("Magic: " + n);
-            System.out.println("Health: " + n);
+            System.out.println("Strength: " + Strength);
+            System.out.println("Magic: " + Magic);
+            System.out.println("Health: " + Health*3);
         }
         System.out.println();
         System.out.println("Happy adventuring!");
-
 
 
     }
